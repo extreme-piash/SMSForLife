f0a6dd635f4f359dc9ff6156f59ad94d6275c3ce
Mehedi Hassan Piash <piash599@gmail.com>
2016-10-06 11:40:15 +0600

    added abstract class for database

---
+++ app/src/main/java/mpsoftware/ltd/smsforlife/database/DatabaseHelper.java
@@ -0,0 +1,17 @@
+package mpsoftware.ltd.smsforlife.database;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteOpenHelper;
+
+/**
+ * Created by piash on 10/6/16.
+ */
+
+public abstract class DatabaseHelper extends SQLiteOpenHelper {
+    private static final String DATABASE_NAME = "smsforlife.db";
+    private static final int DATABASE_VERSION = 1;
+
+    public DatabaseHelper(final Context context) {
+        super(context, DATABASE_NAME, null, DATABASE_VERSION);
+    }
+}
